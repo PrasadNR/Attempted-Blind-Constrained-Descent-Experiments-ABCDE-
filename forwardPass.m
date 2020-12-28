5ad44e1da96897807f76db6a4faa57dd4a165e48
Prasad N R <PrasadNR@users.noreply.github.com>
2020-12-28 18:13:52 -0500

    pooling

--- forwardPass.m
+++ forwardPass.m
@@ -3,6 +3,8 @@ function trainAccuracy = forwardPass (x_train, CNNtable, batch_size)
   for i = 1:batch_size
     x = reshape(x_train(i, :, :, :), size(x_train, 2), size(x_train, 3), size(x_train, 4));
     x = convolution(x, CNNtable, "layer1");
+    x = pooling(x);
+    x = convolution(x, CNNtable, "layer2");
   endfor
   trainAccuracy = x;
   
---
+++ pooling.m
@@ -0,0 +1,13 @@
+function featureMaps = pooling (inputMaps, maxpoolHeight = 2, maxpoolWidth = 2)
+
+  outputSizeH = ceil(size(inputMaps, 1) / maxpoolHeight);
+  outputSizeW = ceil(size(inputMaps, 2) / maxpoolWidth);
+  featureMaps = zeros(outputSizeH, outputSizeW, size(inputMaps, 3));
+  
+  maxpool = @(block_struct) max(block_struct(:));
+  
+  for i = 1:size(inputMaps, 3)
+    featureMaps(:, :, i) = blockproc(inputMaps(:, :, i), [maxpoolHeight, maxpoolWidth], maxpool);
+  endfor
+
+endfunction
