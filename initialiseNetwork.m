8db721140d14a2874670ccc5253c0e8fe56d5ef5
Prasad N R <PrasadNR@users.noreply.github.com>
2020-12-28 10:40:22 -0500

    initialiseNetwork

---
+++ initialiseNetwork.m
@@ -0,0 +1,10 @@
+function CNNtable = initialiseNetwork ()
+
+  CNNtable = struct();
+  CNNtable.layer1 =  unifrnd(-1, 1, [5, 5, 6]);
+  CNNtable.layer2 =  unifrnd(-1, 1, [5, 5, 16]);
+  CNNtable.layer3 =  unifrnd(-1, 1, [5, 5, 120]);
+  CNNtable.layer4 =  unifrnd(-1, 1, [1, 1, 84]);
+  CNNtable.layer5 =  unifrnd(-1, 1, [1, 1, 10]);
+
+endfunction
